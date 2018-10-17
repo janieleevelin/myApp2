cb535aad75f7e31d4538b1c5ed7b7169c6498bf3
janieleevelin <jany681@hotmail.com>
2018-10-17 14:26:40 -0300

    olá

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { LojasPage } from '../pages/lojas/lojas';
+import { ContatoPage } from '../pages/contato/contato';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CardapioPage } from '../pages/cardapio/cardapio';
+import { MensagemPage } from '../pages/mensagem/mensagem';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    LojasPage,
+    ContatoPage,
+    TabsPage,
+    CardapioPage,
+    MensagemPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +28,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    LojasPage,
+    ContatoPage,
+    TabsPage,
+    CardapioPage,
+    MensagemPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CardapioPage } from '../cardapio/cardapio';
+import { LojasPage } from '../lojas/lojas';
+import { ContatoPage } from '../contato/contato';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = CardapioPage;
+  tab2Root = LojasPage;
+  tab3Root = ContatoPage;
 
   constructor() {
 
